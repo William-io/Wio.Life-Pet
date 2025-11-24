d085808e30c9ada753aafb3980694e18e86f0639
William-io <capuletos@live.com>
2025-11-24 16:18:19 -0300

    Feat(App): #006 - Adicionada funcionalidade de logout e exibição do nome de usuário na topbar

--- Source/Wio-Life-Pet.App/src/app/settings/service/auth.ts
+++ Source/Wio-Life-Pet.App/src/app/settings/service/auth.ts
@@ -2,13 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { URL_API } from '../api/config';
 import { sha512 } from 'js-sha512';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Auth {
 
-  constructor(private http: HttpClient) { }
+  constructor(private router: Router, private http: HttpClient) { }
 
   isLoggedIn(): boolean {
     let token = localStorage.getItem('token_sigvet');
@@ -17,6 +18,12 @@ export class Auth {
     }
 
     return true;
+  }
+
+  logout(): void{
+    localStorage.removeItem('token_sigvet');
+    localStorage.removeItem('user_vet');
+    this.router.navigate(['/auth/login']);
   }
 
   login(data: {username: string, passwordHash: string}) {
